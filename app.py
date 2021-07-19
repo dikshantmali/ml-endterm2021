95fa773fd2beb5fb9793f44765f66e2999891a6f
dikshant mali <47856779+dikshantmali@users.noreply.github.com>
2021-07-19 14:42:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,9 +59,9 @@ def main():
    <div class="" style="background-color:#fa8072" >
    <div class="clearfix">           
    <div class="col-lg-12">
-   <center><p style="font-size:40px;color:black;margin-top:10px;">Midterm 1(Dikshant Mali)</p></center> 
+   <center><p style="font-size:40px;color:black;margin-top:10px;">RTU EndTerm Examination Dikshant Mali</p></center> 
    <center><p style="font-size:30px;color:black;margin-top:10px;">Department of Computer Engineering PIET,Jaipur</p></center> 
-   <center><p style="font-size:25px;color:black;margin-top:10px;"Machine Learning Lab </p></center> 
+   <center><p style="font-size:25px;color:black;margin-top:10px;"PIET18CS049 </p></center> 
    </div>
    </div>
    </div>
